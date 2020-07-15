a2c986f3cd8b959601160f820ace8dae2a9cb5e6
framaz <framaz@yandex.ru>
2020-07-16 00:55:17 +0500

    feat: data models for pixel storage

--- models.py
+++ models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class PixelHistory(models.Model):
+    x = models.IntegerField(null=False)
+    y = models.IntegerField(null=False)
+    color = models.CharField(max_length=7, default="#000000")
+    date = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey('auth.User', on_delete=models.DO_NOTHING)
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(check=models.Q(x__gte=0), name="x_gte_0"),
+            models.CheckConstraint(check=models.Q(y__gte=0), name="y_gte_0"),
+        ]
+
+
+class FieldHistory(models.Model):
+    field = models.BinaryField()
+    date = models.DateTimeField(auto_now=True)
+
+
+class CurrentField(models.Model):
+    x = models.IntegerField(null=False)
+    y = models.IntegerField(null=False)
+    color = models.CharField(max_length=7, default="#000000")
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['x', 'y'], name="Primary Key"),
+        ]
--- views.py
+++ views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from yet_another_pixel_draw import serializers
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
+class UpdatePixel(APIView):
+    def put(self, request, format=None):
+        serializer = serializers.CurrentFieldSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
