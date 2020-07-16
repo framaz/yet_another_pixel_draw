b087064e088e7f2352f0aae0a84996339f94f550
framaz <framaz@yandex.ru>
2020-07-16 13:10:04 +0500

    feat: authentication check for pixel creators

--- serializers.py
+++ serializers.py
@@ -1,21 +1,28 @@
 from rest_framework import serializers
 from yet_another_pixel_draw import models
 from django.core.exceptions import ObjectDoesNotExist
-from django.core.cache import caches
+
 
 class CurrentFieldSerializer(serializers.ModelSerializer):
-    def save(self):
+    def save(self, user):
+        x = self.validated_data['x']
+        y = self.validated_data['y']
+        color = self.validated_data['color']
+        models.PixelHistory.objects.create(x=x, y=y, color=color, user=user)
         try:
-            x = self.validated_data['x']
-            y = self.validated_data['y']
             res = models.CurrentField.objects.all().filter(x=x, y=y)[0]
             self.update(res, validated_data=self.validated_data)
         except ObjectDoesNotExist:
             super().save()
 
-
     class Meta:
         model = models.CurrentField
         fields = ['x', 'y', 'color']
+
+
+class GetGridSerialiser(serializers.Serializer):
+    x = serializers.IntegerField()
+    y = serializers.IntegerField()
+    size = serializers.IntegerField()
 
 # {"x": 1, "y": 1, "color": "#111111"}
--- views.py
+++ views.py
@@ -3,13 +3,16 @@ from rest_framework.views import APIView
 from yet_another_pixel_draw import serializers
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 class UpdatePixel(APIView):
+    permission_classes = [IsAuthenticated]
+
     def put(self, request, format=None):
         serializer = serializers.CurrentFieldSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
+            serializer.save(request.user)
             return Response(status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
