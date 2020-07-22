a1756ab231024bbdd78471d91d15d596b1517698
framaz <framaz@yandex.ru>
2020-07-22 21:13:57 +0500

    refactor: docstrings and small other refactoring for views.
    Signed-off-by: framaz <framaz@yandex.ru>

--- urls.py
+++ urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('change_color/', views.UpdatePixel.as_view(), name='change_color'),
     path('upload_picture/', views.UploadPicture.as_view(), name='upload_picture'),
-    path('grid/<int:size>/<int:x>/<int:y>/', views.GetPixelFromGrid.as_view(), name='get_from_grid'),
+    path('grid/<int:size>/<int:x>/<int:y>/', views.GetGridElement.as_view(), name='get_from_grid'),
     path('grid/', views.GetGridSize.as_view(), name='get_grid_size'),
     path('history/', views.GetHistory.as_view(), name='get_history'),
     path('', views.FrontEnd.as_view(), name="frontend")
--- views.py
+++ views.py
@@ -1,3 +1,8 @@
+"""Views.
+
+Mostly just apis. No logic is here, logic is delegated to serializers.
+"""
+
 from json import dumps
 
 from django.core.cache import cache
@@ -18,9 +23,19 @@ from yet_another_pixel_draw import serializers
 
 # Create your views here.
 class UpdatePixel(APIView):
+    """Api for pixel updating.
+
+    Needs the user to be authenticated."""
     permission_classes = [IsAuthenticated]
 
     def put(self, request, format=None):
+        """Update a pixel.
+
+        Also notifies all websocket clients about pixel change.
+
+        Returns:
+            Operation code status.
+        """
         serializer = serializers.CurrentFieldSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save(request.user)
@@ -35,10 +50,19 @@ class UpdatePixel(APIView):
 
 
 class UploadPicture(APIView):
+    """New field picture upload api.
+
+    Accessible only by admin. Requires a file passed.
+    """
     permission_classes = [IsAdminUser]
     parser_classes = (FileUploadParser,)
 
     def put(self, request, format=None):
+        """Upload new field picture.
+
+        Returns:
+            Operation code status.
+            """
         serializer = serializers.NewFieldSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save(request.user)
@@ -49,8 +73,19 @@ class UploadPicture(APIView):
 
 GRID_CACHING = 60
 
+
 class GetHistory(APIView):
+    """Get pixel change from date history api.
+
+    Not used now but might become useful if grid elements become cachable.
+    """
     def get(self, request):
+        """Get pixel history from date.
+
+        Returns:
+            JSON-formatted pixel change history or error code.
+                Example: {"x": 1, "y": 2, "color": "[230, 255, 255]", "date": "20050809T183142+03"}
+        """
         date = request.query_params['date']
         serializer = serializers.PixelHistorySerializer(data={'date': date})
         serializer = serializer
@@ -58,10 +93,13 @@ class GetHistory(APIView):
         return Response(res, status=status.HTTP_200_OK)
 
 
+class GetGridElement(APIView):
+    """Api for getting grid elements.
 
-class GetPixelFromGrid(APIView):
-    #@method_decorator(cache_page(GRID_CACHING))
-    def get(self, request, size, x, y, format=None):
+    Now it's not cachable."""
+    # @method_decorator(cache_page(GRID_CACHING))
+    def get(self, request, size: int, x: int, y: int, format=None):
+        """Get grid element by it's layer and (x, y) coordinates."""
         serializer = serializers.GetGridSerializer(data={"x": x, "size": size, "y": y})
         if serializer.is_valid():
             field = serializer.get_proper_field()
@@ -72,8 +110,16 @@ class GetPixelFromGrid(APIView):
 
 
 class GetGridSize(APIView):
-    #@method_decorator(cache_page(GRID_CACHING))
+    """Api for getting grid sizes.
+
+    It does not explicitly list all available grid elements with links to them, but it is easy to find
+    by the grid sizes.
+
+    Now it's not cachable.
+    """
+    # @method_decorator(cache_page(GRID_CACHING))
     def get(self, request, format=None):
+        """Get grid sizes."""
         res = {}
         for i in range(serializers.MAX_GRID_SIZE):
             res[i] = cache.get(f"grid_size_{i}")
@@ -82,4 +128,5 @@ class GetGridSize(APIView):
 
 
 class FrontEnd(TemplateView):
+    """Frontend view, just renders frontend template."""
     template_name = "yapd/front_end.html"
