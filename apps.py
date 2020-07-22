68f89e46f17f2ebe434f5a3edbf55b316bc7543d
framaz <framaz@yandex.ru>
2020-07-22 18:36:58 +0500

    refactor: docstrings, comments and styling for app start file
    Signed-off-by: framaz <framaz@yandex.ru>

--- apps.py
+++ apps.py
@@ -1,20 +1,33 @@
 from django.apps import AppConfig
 from django.core.cache import cache
 
+
 class YetAnotherPixelDrawConfig(AppConfig):
     name = 'yet_another_pixel_draw'
+
     def ready(self):
+        """Load the pixel field from db to caches.
+
+        Note that only the 0 layer grid is loaded, all other levels are rebuild from scratch.
+        As there may be that a field grid was saved before new pixels were added to it, new pixels should
+        be added.
+        Also don't move the imports to the top of the file as django throws exceptions if you try to
+        import models before django is fully started.
+
+        """
         from .models import CurrentField
         from .serializers import MAX_GRID_SIZE, SMALLEST_SQUARE_SIZE, PixelHistorySerializer, img_split_and_cache
         from django.core.cache import cache
         from django.db.models import Min
         import numpy as np
         import json
-        import matplotlib.pyplot as plt
 
         objects = CurrentField.objects.all().order_by('size')
         grid_size = []
-        grids = np.ndarray(shape=(MAX_GRID_SIZE ,256, 256), dtype=object)
+        grids = np.ndarray(shape=(MAX_GRID_SIZE, 256, 256), dtype=object)
+
+        # loading the 0 layer from db
+        # TODO only 0 layer
 
         for i in range(MAX_GRID_SIZE):
             grid_size.append([-1, -1])
@@ -26,10 +39,14 @@ class YetAnotherPixelDrawConfig(AppConfig):
             grid = np.fromstring(obj.image, dtype=np.uint8, ).reshape((SMALLEST_SQUARE_SIZE, SMALLEST_SQUARE_SIZE, 3))
             grids[obj.size, obj.x, obj.y] = {'grid': grid, 'date': obj.date}
 
+        # calculating grid sizes
+
         for i in range(MAX_GRID_SIZE):
             for j in range(2):
                 grid_size[i][j] += 1
             cache.set(f'grid_size_{i}', grid_size[i])
+
+        # Adding newer pixels to layer 0 grids
 
         min_date = CurrentField.objects.all().aggregate(Min('date'))
         min_date = min_date['date__min']
@@ -41,6 +58,8 @@ class YetAnotherPixelDrawConfig(AppConfig):
             y_grid, y_in_grid = divmod(pixel.y, SMALLEST_SQUARE_SIZE)
             grids[0, x_grid, y_grid]['grid'][x_in_grid][y_in_grid] = np.array(json.loads(pixel.color), dtype=np.uint8)
 
+        # recalculating all other grids
+
         shape = list(np.array(grid_size[0]) * SMALLEST_SQUARE_SIZE)
         shape.append(3)
         grid_image = np.zeros(shape=shape, dtype=np.uint8)
@@ -49,8 +68,7 @@ class YetAnotherPixelDrawConfig(AppConfig):
             for y in range(grid_size[0][1]):
                 cache.set(f'level0_{x}_{y}', grids[0][x][y]['grid'])
                 grid_image[x * SMALLEST_SQUARE_SIZE: (x + 1) * SMALLEST_SQUARE_SIZE,
-                           y * SMALLEST_SQUARE_SIZE: (y + 1) * SMALLEST_SQUARE_SIZE] = grids[0][x][y]['grid']
+                y * SMALLEST_SQUARE_SIZE: (y + 1) * SMALLEST_SQUARE_SIZE] = grids[0][x][y]['grid']
 
         # grid_image = grid_image.transpose(1, 0, 2)
         img_split_and_cache(grid_image)
-
